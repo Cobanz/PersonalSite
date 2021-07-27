ad4208938c0f8cd7d899215cf233fe64aa92fa5a
Charles Grant Nichols <grant@inkdots.com>
2021-07-26 19:56:49 -0500

    adjusted personal site, pushed to live

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
             <div className="project_container">
             <br/>
             <br/>
-                <div><a target="_blank" href="https://github.com/Cobanz/Portfolio/tree/main/portfolio">This Portfolio</a></div>
+                <div><a target="_blank" href="https://github.com/Cobanz/PersonalSite">This Portfolio</a></div>
                 <br/>
                 <div><a target="_blank" href="https://github.com/Cobanz/Biggles-Bottomless-Emporium">Biggles Bottomless Emporium</a></div>
                 <br/>
