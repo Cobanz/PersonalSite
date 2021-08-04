99f7cc11d46eb0464322094c783b96abc922514f
Charles Grant Nichols <grant@inkdots.com>
2021-08-04 12:53:37 -0500

    added hackathon

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -10,11 +10,13 @@ function Projects() {
             <div className="project_container">
             <br/>
             <br/>
-                <div><a target="_blank" href="https://github.com/Cobanz/PersonalSite">This Portfolio</a></div>
+                <div><a target="_blank" href="https://github.com/Cobanz/PersonalSite">This Portfolio</a> - going to really try and use bootstraps and css to make a nice looking site</div>
                 <br/>
-                <div><a target="_blank" href="https://github.com/Cobanz/Biggles-Bottomless-Emporium">Biggles Bottomless Emporium</a></div>
+                <div><a target="_blank" href="https://github.com/Cobanz/Biggles-Bottomless-Emporium">Biggles Bottomless Emporium</a> - Solo project that I went super in-depth with my backend and front end data manipulation with lots of room to grow </div>
                 <br/>
-                <div><a target="_blank" href="https://github.com/Cobanz/Space-Cowboy-Game-Phase4">Space Cowboy</a></div>
+                <div><a target="_blank" href="https://github.com/Cobanz/Space-Cowboy-Game-Phase4">Space Cowboy</a> - group project using a new library - kaboom.js- and adapting it to be used in React and still extracting the data.</div>
+                <br/>
+                <div><a target="_blank" href="https://github.com/Cobanz/CardGame">Mintbean Hackathon</a> - war card-game, using a external api to store all the card deck and do the deck manipulations </div>
             </div>
         </div>
     )
